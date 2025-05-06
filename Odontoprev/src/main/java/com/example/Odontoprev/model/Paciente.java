aa1f7a786da49ea6004b5ccb2e1da9b91cbd6cdc
GiovannaGiantomaso <giovanna1205@live.com>
2025-05-06 13:50:41 -0300

    utilizando lombok na model paciente

--- Odontoprev/src/main/java/com/example/Odontoprev/model/Paciente.java
+++ Odontoprev/src/main/java/com/example/Odontoprev/model/Paciente.java
@@ -1,17 +1,24 @@
 package com.example.Odontoprev.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table(name = "PACIENTE", schema = "RM553369")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paciente implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PACIENTE")
     @SequenceGenerator(name = "SEQ_PACIENTE", sequenceName = "SEQ_PACIENTE", allocationSize = 1)
-
     @Column(name = "ID_PACIENTE")
     private Long id;
 
@@ -22,7 +29,6 @@ public class Paciente implements Serializable {
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataNascimento;
-
 
     @Column(name = "TELEFONE", length = 15)
     private String telefone;
@@ -36,60 +42,4 @@ public class Paciente implements Serializable {
     @ManyToOne
     @JoinColumn(name = "ID_ENDERECO", referencedColumnName = "ID_ENDERECO")
     private Endereco endereco;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getIdGenero() {
-        return idGenero;
-    }
-
-    public void setIdGenero(Integer idGenero) {
-        this.idGenero = idGenero;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
 }
